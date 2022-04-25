bfdcb74006209ac2710352c90c4d922cc0be93c5
sak <saknarak@gmail.com>
2022-04-25 14:02:16 +0700

    add template and signin

---
+++ src/pages/signin.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <form>
+      <div>
+        Sign-in
+      </div>
+      <div>
+        <label for="username">Username</label>
+        <input type="text" id="username" value="User1">
+      </div>
+      <div>
+        <label for="password">Password</label>
+        <input type="password" id="password" value="Pass1">
+      </div>
+      <div>
+        <input type="checkbox" id="remember">
+        <label for="remember">Remember username?</label>
+      </div>
+      <div>
+        <button type="button">
+          Sign-In
+        </button>
+        <button type="reset">
+          Reset
+        </button>
+      </div>
+    </form>
+  </div>
+</template>
