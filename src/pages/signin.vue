315d69953dedd539bebfa0e5b68de254d47de6a4
sak <saknarak@gmail.com>
2022-04-25 15:25:52 +0700

    add v-model computed methods

--- src/pages/signin.vue
+++ src/pages/signin.vue
@@ -2,23 +2,26 @@
   <div>
     <form>
       <div>
-        Sign-in
+        Sign-in busy={{ busy }}
       </div>
       <div>
         <label for="username">Username</label>
-        <input type="text" id="username" value="User1">
+        <input type="text" id="username" v-model="username">
+        {{ username }}
       </div>
       <div>
         <label for="password">Password</label>
-        <input type="password" id="password" value="Pass1">
+        <input type="text" id="password" v-model="password">
+        {{ password }}
       </div>
       <div>
-        <input type="checkbox" id="remember">
+        <input type="checkbox" id="remember" v-model="remember">
         <label for="remember">Remember username?</label>
+        {{ remember }}
       </div>
       <div>
-        <button type="button">
-          Sign-In
+        <button type="button" :disabled="lock" @click="signin">
+          Sign-In {{ lock }}
         </button>
         <button type="reset">
           Reset
@@ -27,3 +30,43 @@
     </form>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      username: '',
+      password: '',
+      remember: false,
+      ok: true,
+      busy: false,
+    }
+  },
+
+  computed: {
+    lock() {
+      return this.username === '' || this.password === ''
+    },
+  },
+
+  // created() {
+  //   console.log('created')
+  //   let n = 1
+  //   setInterval(() => {
+  //     this.username = 'somsak' + n
+  //     n++
+  //     this.remember = !this.remember
+  //     console.log('oooookkkkkkk')
+  //   }, 1000)
+  // },
+
+  methods: {
+    signin() {
+      this.busy = true
+      setTimeout(() => {
+        this.busy = false
+      }, 3000)
+    },
+  },
+}
+</script>
