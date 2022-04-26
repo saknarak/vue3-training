d52a8b7a6dc9017ff9dde864a7d00a405d58d383
sak <saknarak@gmail.com>
2022-04-26 16:07:27 +0700

    add logo

--- src/pages/signin.vue
+++ src/pages/signin.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="flex justify-center items-center min-h-screen">
+  <div class="flex flex-col gap-4 justify-center items-center min-h-screen">
+    <div class="w-20 h-20 bg-pink-300 flex justify-center items-center rounded-md">
+      LOGO
+    </div>
     <signin-box
       @success="signinSuccess"
       @error="singinError"
