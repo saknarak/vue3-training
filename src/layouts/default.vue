09645f902b1f60ee19f1e65263804a61f1ac1efe
sak <saknarak@gmail.com>
2022-04-29 14:20:05 +0700

    windi css

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -10,32 +10,32 @@ import axios from 'axios'
 import { store } from '../store/store.js'
 
 export default {
-  async beforeRouteEnter(from, to, next) {
-    let token = localStorage.getItem('token')
-    if (!token) {
-      return next('/signin')
-    }
-    // GET PROFILE
-    axios.defaults.headers.common.Authorization = `Bearer ${token}`
-    // axios.defaults.headers.common['X-Powered-By'] = 'Somsak'
+  // async beforeRouteEnter(from, to, next) {
+  //   let token = localStorage.getItem('token')
+  //   if (!token) {
+  //     return next('/signin')
+  //   }
+  //   // GET PROFILE
+  //   axios.defaults.headers.common.Authorization = `Bearer ${token}`
+  //   // axios.defaults.headers.common['X-Powered-By'] = 'Somsak'
 
-    let { data } = await axios.get('/api/profile')
-    if (!data.profile) {
-      return next('/signin')
-    }
-    store.profile = data.profile
-    next()
-  },
+  //   let { data } = await axios.get('/api/profile')
+  //   if (!data.profile) {
+  //     return next('/signin')
+  //   }
+  //   store.profile = data.profile
+  //   next()
+  // },
 
   created() {
-    this.tokenTimer = setInterval(async () => {
-      let token = localStorage.getItem('token')
-      let newToken = await axios.post('/api/auth/token')
-      if (newToken && newToken !== token) {
-        localStorage.setItem('token', newToken)
-        axios.defaults.headers.common.Authorization = `Bearer ${newToken}`
-      }
-    }, 5 * 60 * 1000)
+    // this.tokenTimer = setInterval(async () => {
+    //   let token = localStorage.getItem('token')
+    //   let newToken = await axios.post('/api/auth/token')
+    //   if (newToken && newToken !== token) {
+    //     localStorage.setItem('token', newToken)
+    //     axios.defaults.headers.common.Authorization = `Bearer ${newToken}`
+    //   }
+    // }, 5 * 60 * 1000)
   },
   beforeUnmount() {
     clearInterval(this.tokenTimer)
--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,6 +1,29 @@
 <template>
-  <h1>WELCOME {{ store.profile?.displayName }}</h1>
+  <h1 class="bg-white text-black dark:(bg-black text-white)">
+    WELCOME {{ store.profile?.displayName }}
+  </h1>
+  <div class="dark">
+    <h1 class="bg-white text-black dark:(bg-black text-white)">
+      WELCOME {{ store.profile?.displayName }}
+    </h1>
+  </div>
+  <div class="flex flex-row flex-wrap">
+    <div class="h-20 w-full sm:(w-1/2) lg:(w-1/3) rounded-lg bg-yellow-100"></div>
+    <div class="h-20 w-full sm:(w-1/2) lg:(w-1/3) rounded-lg bg-blue-100"></div>
+    <div class="h-20 w-full lg:(w-1/3) rounded-lg bg-red-100"></div>
+  </div>
+  <input type="text" required pattern="^[0-9]+$" class="my-input">
+  <div class="text-red-500">
+    TEXT: {{ text }}
+  </div>
+  <button class="btn">
+    BUTTON1
+  </button>
+  <button class="btn">
+    BUTTON2
+  </button>
   <div>Last Login: {{ store.profile?.lastLogin }}</div>
+
   <!-- <div>{{ store }}</div> -->
 </template>
 <script>
@@ -9,7 +32,40 @@ export default {
   data() {
     return {
       store,
+      text: '',
     }
+  },
+  created() {
+    this.evtSource = new EventSource('/api/data')
+    this.evtSource.onmessage = this.onEventMessage
+  },
+  beforeUnmount() {
+    this.evtSource.close()
+  },
+  methods: {
+    onEventMessage(evt) {
+      console.log(evt.data)
+      let obj = JSON.parse(evt.data)
+      console.log(obj)
+      this.text = obj.ts
+    },
   },
 }
 </script>
+<style>
+.xxx {
+  background-color: yellow;
+}
+.xxx:hover {
+  border: 1px solid red;
+}
+.my-input {
+  @apply p-1 bg-blue-400 invalid:(bg-red-300);
+}
+.btn {
+  @apply py-2 px-4 font-semibold rounded-lg shadow-md bg-green-300;
+  .text {
+    @apply text-pink-200;
+  }
+}
+</style>
